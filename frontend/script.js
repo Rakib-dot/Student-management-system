8fcf04ba12c54e7fae4dedfdbe25d0c5332f01ac
rakib <u1804107@student.cuet.ac.bd>
2023-10-11 20:32:30 +0600

    First commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,7 @@
-// JavaScript code to handle form submission, fetch students, and display them
 const createStudentForm = document.getElementById("createStudentForm");
 const updateStudentForm = document.getElementById("updateStudentForm");
 const deleteStudentForm = document.getElementById("deleteStudentForm");
 const studentList = document.getElementById("studentList");
-const messageElement = document.getElementById("message");
-
-function showMessage(message, success = true) {
-    messageElement.textContent = message;
-    if (success) {
-        messageElement.style.color = "green";
-    } else {
-        messageElement.style.color = "red";
-    }
-}
-
-// Function to fetch and display students (GET request)
-function fetchStudents() {
-    // Send a GET request to retrieve the list of students
-    fetch("http://localhost:8080/getAllStudent")
-        .then(response => response.json())
-        .then(students => {
-            studentList.innerHTML = ""; // Clear the existing list
-            students.forEach(student => {
-                const listItem = document.createElement("li");
-                listItem.textContent = `Roll: ${student.roll}, Name: ${student.name}, Address: ${student.address}`;
-                listItem.setAttribute("data-roll", student.roll); // Store the roll number as a data attribute
-                studentList.appendChild(listItem);
-            });
-        })
-        .catch(error => console.error(error));
-}
 
 // Code for creating students (POST request)
 createStudentForm.addEventListener("submit", function (event) {
@@ -38,6 +10,8 @@ createStudentForm.addEventListener("submit", function (event) {
     const roll = document.getElementById("roll").value;
     const name = document.getElementById("name").value;
     const address = document.getElementById("address").value;
+
+    const createMessage = document.getElementById("createMessage"); // Select the message element for create form
 
     // Create a JSON object representing the student data
     const studentData = {
@@ -54,29 +28,28 @@ createStudentForm.addEventListener("submit", function (event) {
         },
         body: JSON.stringify(studentData)
     })
-    .then(response => response.json())
-    .then(data => {
-        if (typeof data === 'string') {
-            // If the response is a string, it's an error message
-            showMessage(data, false);
-        } else {
-            // If the response is an object, it's a student object
-            showMessage("Student created successfully", true);
-
+    .then(response => {
+        if (response.ok) {
+            // If the response status is OK, it means the student was created successfully
+            createMessage.textContent = "Student created successfully"; // Update the create form message
             // Add the new student to the list
             const listItem = document.createElement("li");
-            listItem.textContent = `Roll: ${data.roll}, Name: ${data.name}, Address: ${data.address}`;
-            listItem.setAttribute("data-roll", data.roll);
+            listItem.textContent = `Roll: ${studentData.roll}, Name: ${studentData.name}, Address: ${studentData.address}`;
+            listItem.setAttribute("data-roll", studentData.roll);
             studentList.appendChild(listItem);
-
-            // Clear the form
+            // After successful student creation, clear the form data
             document.getElementById("roll").value = "";
             document.getElementById("name").value = "";
             document.getElementById("address").value = "";
+
+        } else {
+            // Handle the case where the student creation failed (you can set an error message)
+            createMessage.textContent = "Student with the same roll number already exists."; // Example error message
         }
     })
     .catch(error => console.error(error));
 });
+
 
 // Code for updating students (PUT request)
 updateStudentForm.addEventListener("submit", function (event) {
@@ -86,6 +59,9 @@ updateStudentForm.addEventListener("submit", function (event) {
     const updateName = document.getElementById("updateName").value;
     const updateAddress = document.getElementById("updateAddress").value;
 
+    const updateMessage = document.getElementById("updateMessage"); // Select the message element for update form
+
+    // Create a JSON object representing the updated student data
     const updatedStudentData = {
         roll: parseInt(updateRoll),
         name: updateName,
@@ -100,29 +76,28 @@ updateStudentForm.addEventListener("submit", function (event) {
         },
         body: JSON.stringify(updatedStudentData)
     })
-    .then(response => response.json())
-    .then(data => {
-        if (typeof data === 'string') {
-            // If the response is a string, it's an error message
-            showMessage(data, false);
-        } else {
-            // If the response is an object, it's a student object
-            showMessage("Student updated successfully", true);
-
+    .then(response => {
+        if (response.ok) {
+            // If the response status is OK, it means the update was successful
+            updateMessage.textContent = "Student updated successfully"; // Update the update form message
             // Update the student's details in the list
-            const studentListItem = document.querySelector(`li[data-roll="${data.roll}"]`);
+            const studentListItem = document.querySelector(`li[data-roll="${updatedStudentData.roll}"]`);
             if (studentListItem) {
-                studentListItem.textContent = `Roll: ${data.roll}, Name: ${data.name}, Address: ${data.address}`;
+                studentListItem.textContent = `Roll: ${updatedStudentData.roll}, Name: ${updatedStudentData.name}, Address: ${updatedStudentData.address}`;
             }
-
-            // Clear the form
+            // After successful student update, clear the form data
             document.getElementById("updateRoll").value = "";
             document.getElementById("updateName").value = "";
             document.getElementById("updateAddress").value = "";
+
+        } else {
+            // Handle the case where the update failed (you can set an error message)
+            updateMessage.textContent = "Enter valid information"; // Example error message
         }
     })
     .catch(error => console.error(error));
 });
+
 
 // Code for deleting students (DELETE request)
 deleteStudentForm.addEventListener("submit", function (event) {
@@ -130,29 +105,45 @@ deleteStudentForm.addEventListener("submit", function (event) {
 
     const deleteRoll = document.getElementById("deleteRoll").value;
 
+    const deleteMessage = document.getElementById("deleteMessage"); // Select the message element for delete form
+
     // Send a DELETE request to delete a student
     fetch(`http://localhost:8080/deleteStudent/${deleteRoll}`, {
         method: "DELETE"
     })
     .then(response => {
         if (response.ok) {
-            showMessage("Student deleted successfully", true);
-
+            deleteMessage.textContent = "Student deleted successfully"; // Update the delete form message
             // Remove the deleted student from the list
             const studentListItem = document.querySelector(`li[data-roll="${deleteRoll}"]`);
             if (studentListItem) {
                 studentListItem.remove();
             }
-
-            // Clear the form
+            // After successful student deletion, clear the form data
             document.getElementById("deleteRoll").value = "";
-        } else {
-            showMessage(data, false);
+
+        }
+        else {
+            deleteMessage.textContent = "Student not found"; // Update the delete form message
         }
     })
     .catch(error => console.error(error));
 });
 
 // Fetch and display students when the page loads
+function fetchStudents() {
+    // Send a GET request to retrieve the list of students
+    fetch("http://localhost:8080/getAllStudent")
+        .then(response => response.json())
+        .then(students => {
+            studentList.innerHTML = ""; // Clear the existing list
+            students.forEach(student => {
+                const listItem = document.createElement("li"); //For each student, it creates a new list item element (<li>)
+                listItem.textContent = `Roll: ${student.roll}, Name: ${student.name}, Address: ${student.address}`;
+                listItem.setAttribute("data-roll", student.roll); // Store the roll number as a data attribute
+                studentList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error(error));
+}
 fetchStudents();
-
